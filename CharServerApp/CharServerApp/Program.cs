26ff5de9e8f9ce043138ab5866f2ca31612f1a68
Fabian Astorga <fabian.astorga02@inacapmail.cl>
2021-04-07 20:11:51 -0400

    Agrega funcionalidad base del servidor

--- CharServerApp/CharServerApp/Program.cs
+++ CharServerApp/CharServerApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CharServerApp.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,37 @@ namespace CharServerApp
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en el puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando conexion de Cliente ....");
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Cliente Conectado");
+                        Console.WriteLine("S: Hola Cliente");
+                        servidor.Escribir("Hola Cliente!");
+                        string mensaje = servidor.Leer();
+                        Console.WriteLine("C: {0}", mensaje );
+                        servidor.CerrarConexion();
+                    }
+
+                }               
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error al iniciar el servidor");
+                Console.ReadKey();
+
+            }
+
         }
     }
 }
